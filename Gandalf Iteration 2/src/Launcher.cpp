2e0f4d8bdc3830ef779c051f41f2aa438eec85f1
ZhihanTuo <johntuozh@gmail.com>
2022-10-14 18:16:27 -0700

    Made a few comments on everything

--- Gandalf Iteration 2/src/Launcher.cpp
+++ Gandalf Iteration 2/src/Launcher.cpp
@@ -1,6 +1,8 @@
 #include "vex.h"
 #include "robot-config.h"
 
+//shoots outward
+
 void fly_wheel_one_forward()
 {
   fly_wheel_one.spin(fwd, 450, pct);
@@ -10,6 +12,8 @@ void fly_wheel_two_forward()
 {
   fly_wheel_two.spin(fwd, 450, pct);
 }
+
+//spins flywheels backwards
 
 void fly_wheel_one_reverse()
 {
